860410ab128f64ac122557fdfd459c9aeb705038
Weihang Zheng <54783950+xXWeIsLyXx@users.noreply.github.com>
2022-07-18 13:16:15 -0400

    Add files via upload

---
+++ Courses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Courses
+//
+//  Created by Weihang Zheng on 2022-07-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
